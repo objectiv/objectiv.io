4ad7c55d689dcf602aa103e520e99339e8141e19
mike <mike@objectiv.io>
2021-12-03 13:59:56 +0100

    check if file exists, clean up

--- env_config.js
+++ env_config.js
@@ -1,16 +1,19 @@
 const fs = require('fs')
 const dotenv = require('dotenv')
 
-
 module.exports = (environment) => {
   const nodeEnv = process.env.NODE_ENV;
 
   // only allow yarn start (dev) in dev mode
-
   if (nodeEnv === 'development' && environment !== 'development'){
     throw new Error(`Not allowed to use 'yarn start' (${nodeEnv}) on non dev build 
       (OBJECTIV_ENVIRONMENT=${process.env.OBJECTIV_ENVIRONMENT})`);
   }
     // read env config from .env file
-  return dotenv.parse(fs.readFileSync(`.env.${environment}`));
+  const filename = `.env.${environment}`;
+  if  ( fs.existsSync(filename) ) {
+    return dotenv.parse(fs.readFileSync(filename));
+  } else {
+    throw new Error(`Couldn't open config for ${environment} (${filename})`);
+  }
 }
