f0f124620259b21a60b686f2d5f8f512a1f1cecc
Ivar Pruijn <ivar@objectiv.ai>
2021-12-10 10:33:29 +0100

    Just log broken links, so they can be fixed later in the Tracking branch

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
   projectName: 'objectiv.io', // Usually your repo name.
 
   onBrokenLinks: 'log',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenMarkdownLinks: 'log',
   trailingSlash: false,
 
   presets: [
