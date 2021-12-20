63c67367054c24d53175cd511c9152023bf42d8e
Ivar Pruijn <ivar@objectiv.ai>
2021-12-20 20:20:25 +0100

    Fix tracker logging setting, and remove spaces in .env files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
     trackerApplicationId: envConfig.trackerApplicationId,
     trackerEndPoint: envConfig.trackerEndPoint,
     slackJoinLink: slackJoinLink,
-    trackerConsoleEnabled: envConfig.trackerConsoleEnabled
+    trackerConsoleEnabled: envConfig.trackerConsoleEnabled === '1'
   },
 
   themeConfig:
@@ -156,5 +156,6 @@ module.exports = config;
 
 console.log("OBJECTIV TRACKER APPLICATION ID:", config.customFields.trackerApplicationId);
 console.log("OBJECTIV TRACKER ENDPOINT:", config.customFields.trackerEndPoint);
+console.log("OBJECTIV TRACKER CONSOLE ENABLED:", config.customFields.trackerConsoleEnabled);
 console.log("DOCUSAURUS URL:", config.baseUrl);
 console.log("DOCUSAURUS BASEURL:", config.baseUrl);
