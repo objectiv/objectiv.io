85f6c8820880f27923af8464460d1246a4ccb8f3
sdirosa <surai@objectiv.io>
2022-01-06 13:46:44 +0100

    Remove useless prop proxying

--- src/theme/NavbarItem/DefaultNavbarItem.tsx
+++ src/theme/NavbarItem/DefaultNavbarItem.tsx
@@ -22,7 +22,6 @@ export default function DefaultNavbarItem({
       Component={OriginalDefaultNavbarItem}
       {...props}
       title={props.title ?? props.label}
-      external={props.external}
     />
   );
 }
