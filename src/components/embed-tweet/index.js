48a5ea094e17b2eaef6afbb3a669cb63fa3e288c
Ivar Pruijn <ivar@objectiv.ai>
2022-02-01 16:31:03 +0100

    Change TrackedContentContext to ContentContentWrapper to get rid of an unnecessary div

--- src/components/embed-tweet/index.js
+++ src/components/embed-tweet/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { TwitterTweetEmbed } from 'react-twitter-embed';
-import { TrackedContentContext } from '@objectiv/tracker-react';
+import { ContentContextWrapper } from '@objectiv/tracker-react';
 
 function EmbedTweet({
   id
 }) {
   return (
-    <TrackedContentContext Component={'div'} id={id}>
+    <ContentContextWrapper id={id}>
       <TwitterTweetEmbed 
         tweetId={id}
       />
-    </TrackedContentContext>
+    </ContentContextWrapper>
   );
 }
 
