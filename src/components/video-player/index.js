57109d87ae84bbb6b6973099f3a1f94bf025d2bd
Ivar Pruijn <ivar@objectiv.ai>
2022-02-01 17:58:51 +0100

    Attempt to fix the issues with Vimeo video player not showing because it's blocked by cookiebot

--- src/components/video-player/index.js
+++ src/components/video-player/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import ReactPlayer from "react-player";
+import ReactPlayer from "react-player/vimeo";
 import {
   MediaPlayerContextWrapper,
   trackMediaLoadEvent,
@@ -16,25 +16,27 @@ function VideoPlayer({
   id,
   enableControls = true
 }) {
-  const [isPlaying, setPlaying] = useState(false);
-
-  function handlePlayPause() {
-    setPlaying(!isPlaying);
-  }
-
   return (
     <MediaPlayerContextWrapper id={id}>
       {(trackingContext) => (
         <>
           <ReactPlayer
             url={url}
-            playing={isPlaying}
+            config={{
+              vimeo: {
+                dnt: false,
+                playerOptions: {
+                  dnt: false
+                }
+              }
+            }}
             controls={enableControls}
             onReady={() => trackMediaLoadEvent(trackingContext)}
             onStart={() => trackMediaStartEvent(trackingContext)}
             onPause={() => trackMediaPauseEvent(trackingContext)}
             onEnded={() => trackMediaStopEvent(trackingContext)}
             className={clsx(styles.videoWrapper)} 
+            data-cookieconsent="ignore" 
           />
         </>
       )}
