edf10f0f4537257740219f1f45f9556ab55d43fe
Ivar Pruijn <ivar@objectiv.ai>
2022-03-16 12:23:20 +0100

    Revert "Use xdiff from pip install"
    This reverts commit 39cdad45bafd49e9cc34d3a8c2e802e795aa7ad7.

--- docker/build/diff-sitemaps.sh
+++ docker/build/diff-sitemaps.sh
@@ -7,7 +7,7 @@ curl https://objectiv.io/docs/sitemap.xml > ./tmp/$CURRENT_DATE-sitemap-docs.xml
 docker run --rm --entrypoint cat objectiv/website-deploy:$CURRENT_DATE \
   /services/docs/build-production/sitemap.xml > ./tmp/$CURRENT_DATE-build-sitemap-docs.xml
 
-xdiff --compare-files ./tmp/$CURRENT_DATE-sitemap-docs.xml ./tmp/$CURRENT_DATE-build-sitemap-docs.xml
-xdiff --compare-files ./tmp/$CURRENT_DATE-sitemap-website.xml ./tmp/$CURRENT_DATE-build-sitemap-website.xml
+python3 ./docker/build/xdiff.py ./tmp/$CURRENT_DATE-sitemap-docs.xml ./tmp/$CURRENT_DATE-build-sitemap-docs.xml
+python3 ./docker/build/xdiff.py ./tmp/$CURRENT_DATE-sitemap-website.xml ./tmp/$CURRENT_DATE-build-sitemap-website.xml
 
 rm -rf ./tmp/
