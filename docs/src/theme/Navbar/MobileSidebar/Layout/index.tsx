79fc76c7125720a27d47bfd91e87f810b3d1022f
Ivar Pruijn <ivar@objectiv.io>
2022-04-12 21:43:14 +0200

    Fix collision

--- docs/src/theme/Navbar/MobileSidebar/Layout/index.tsx
+++ docs/src/theme/Navbar/MobileSidebar/Layout/index.tsx
@@ -1,36 +1,3 @@
-// import React, {ComponentProps} from 'react';
-// import type LayoutType from '@theme/Navbar/MobileSidebar/Layout';
-// import Layout from '@theme-original/Navbar/MobileSidebar/Layout';
-// import {useNavbarSecondaryMenu} from '@docusaurus/theme-common';
-
-// type Props = ComponentProps<typeof LayoutType>
-
-// // OBJECTIV
-// import { tagOverlay } from '@objectiv/tracker-browser';
-// // OBJECTIV
-
-// export default function LayoutWrapper(props: Props): JSX.Element {
-//   const {shown: secondaryMenuShown} = useNavbarSecondaryMenu();
-
-//   return (
-//     <>
-//       <Layout 
-//         // OBJECTIV
-//         {...tagOverlay({
-//           id: 'hamburger-menu',
-//           options: {
-//             trackVisibility: {
-//               mode: 'manual',
-//               isVisible: secondaryMenuShown
-//             }
-//           }
-//         })}
-//         // END OBJECTIV
-//         {...props} />
-//     </>
-//   );
-// }
-
 import React from 'react';
 import clsx from 'clsx';
 import type {Props} from '@theme/Navbar/MobileSidebar/Layout';
@@ -57,14 +24,14 @@ export default function NavbarMobileSidebarLayout({
             trackVisibility: {
               mode: 'manual',
               isVisible: secondaryMenuShown
-            }
+            },
           }
         })}
-        // END OBJECTIV
         className={clsx('navbar-sidebar__items', {
           'navbar-sidebar__items--show-secondary': secondaryMenuShown,
         })}>
-        <div className="navbar-sidebar__item menu">{primaryMenu}</div>
+        <div className="navbar-sidebar__item menu"></div>
+        {/* END OBJECTIV */}
         <div className="navbar-sidebar__item menu">{secondaryMenu}</div>
       </div>
     </div>
