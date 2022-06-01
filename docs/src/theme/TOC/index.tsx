f897c969f83078d5f25868576561a418a1f89a98
Ivar Pruijn <ivar@objectiv.io>
2022-06-01 21:32:42 +0200

    Migrate TOCs

--- docs/src/theme/TOC/index.tsx
+++ docs/src/theme/TOC/index.tsx
@@ -1,38 +1,14 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+import React, {ComponentProps} from 'react';
+import TOC from '@theme-original/TOC';
+import type TOCType from '@theme/TOC';
+import { TrackedNav } from '@objectiv/tracker-react';
 
-import React from 'react';
-import clsx from 'clsx';
-import type {Props} from '@theme/TOC';
-import TOCItems from '@theme/TOCItems';
-import styles from './styles.module.css';
+type Props = ComponentProps<typeof TOCType>;
 
-// OBJECTIV
-import { tagNavigation } from "@objectiv/tracker-browser";
-// END OBJECTIV
-
-// Using a custom className
-// This prevents TOC highlighting to highlight TOCInline/TOCCollapsible by mistake
-const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
-const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
-
-export default function TOC({className, ...props}: Props): JSX.Element {
+export default function TOCWrapper(props: Props): JSX.Element {
   return (
-    <div
-      className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}
-      // OBJECTIV
-      {...tagNavigation({id: 'toc'})}
-      // END OBJECTIV
-    >
-      <TOCItems
-        {...props}
-        linkClassName={LINK_CLASS_NAME}
-        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
-      />
-    </div>
+    <TrackedNav id={'toc'}>
+      <TOC {...props} />
+    </TrackedNav>
   );
 }
