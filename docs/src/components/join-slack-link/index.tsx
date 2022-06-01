c7a7edf00ff194b1a3876de59dedbb69ac0febac
Ivar Pruijn <ivar@objectiv.io>
2022-06-01 13:21:50 +0200

    Migrate Slack Join component to React Tracker

--- docs/src/components/join-slack-link/index.tsx
+++ docs/src/components/join-slack-link/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Link from '@docusaurus/Link';
-import { tagLink } from '@objectiv/tracker-browser';
+import { TrackedLink } from '../../trackedComponents/TrackedLink';
 
-export default function JoinSlackLink({linkText='join us on Slack', trackingId='join-slack'}) {
+export default function JoinSlackLink({linkText='join us on Slack', trackingId}) {
   const {siteConfig} = useDocusaurusContext();
   const {slackJoinLink} = siteConfig.customFields;
 
-  return <Link 
-    {...tagLink({id: trackingId, href: slackJoinLink as string}) }
-    to={slackJoinLink as string}>
-      {linkText}
-  </Link>;
+  return (
+    <TrackedLink 
+      id={trackingId}  
+      to={slackJoinLink as string}>
+        {linkText}
+    </TrackedLink>
+  );
 };
-
---
+++ docs/src/trackedComponents/TrackedLink.tsx
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2021-2022 Objectiv B.V.
+ */
+
+import Link, {type Props as LinkProps} from '@docusaurus/Link';
+import { TrackedLinkContext, TrackedLinkContextProps } from "@objectiv/tracker-react";
+import React from 'react';
+
+export type TrackedLinkProps = Omit<TrackedLinkContextProps, 'Component' | 'href'> & LinkProps;
+
+export const TrackedLink = React.forwardRef<HTMLAnchorElement, TrackedLinkProps>((props, ref) => (
+  <TrackedLinkContext Component={Link} {...props} href={props.href ?? props.to} forwardHref={!!props.href} ref={ref}/>
+))
