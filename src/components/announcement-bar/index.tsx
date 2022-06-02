350be254480a8a1fac777f95ff2125278de726c8
Ivar Pruijn <ivar@objectiv.io>
2022-06-02 11:53:12 +0200

    Show first icon on mobile as well

--- src/components/announcement-bar/index.tsx
+++ src/components/announcement-bar/index.tsx
@@ -8,13 +8,13 @@ function AnnouncementBar({title, content, ctaLink, ctaText, icon='icon-star'}) {
   return (
     <TrackedDiv id={'announcement-bar'}>
       <div className={clsx(styles.announcement)}>
-        <span className={styles.announcementStar}><img src={"/img/icons/" + icon + ".svg"} alt='star' /></span>
+        <span className={clsx(styles.announcementStar, styles.announcementStarStart)}><img src={"/img/icons/" + icon + ".svg"} alt='star' /></span>
           <strong>{title}</strong>&nbsp;
           {content}&nbsp;
           <TrackedLink to={ctaLink} waitUntilTracked={true}>
             {ctaText}
           </TrackedLink>
-        <span className={styles.announcementStar}><img src={"/img/icons/" + icon + ".svg"} alt='star' /></span>
+        <span className={clsx(styles.announcementStar, styles.announcementStarEnd)}><img src={"/img/icons/" + icon + ".svg"} alt='star' /></span>
       </div>
     </TrackedDiv>
   );
