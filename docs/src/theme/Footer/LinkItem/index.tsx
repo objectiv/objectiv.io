6c68b3463758f403e947b86c91ed6bd8202ba9c1
Ivar Pruijn <ivar@objectiv.io>
2022-06-02 13:36:51 +0200

    Port site footer changes to docs

--- docs/src/theme/Footer/LinkItem/index.tsx
+++ docs/src/theme/Footer/LinkItem/index.tsx
@@ -1,21 +1,23 @@
-import React, {ComponentProps} from 'react';
-import LinkItem from '@theme-original/Footer/LinkItem';
-import type LinkItemType from '@theme/Footer/LinkItem';
-import { TrackedLinkContext } from '@objectiv/tracker-react';
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import { makeIdFromString } from '@objectiv/tracker-core';
+import type LinkItemType from '@theme/Footer/LinkItem';
+import React, { ComponentProps } from 'react';
+import { TrackedLink } from "../../../trackedComponents/TrackedLink";
 
 type Props = ComponentProps<typeof LinkItemType>;
 
 export default function LinkItemWrapper(props: Props): JSX.Element {
   const {item} = props;
-  const label = makeIdFromString(item.label);
-  const href = item.href;
+  const normalizedTo = useBaseUrl(item.to, {forcePrependBaseUrl: true});
+
   return (
-    <TrackedLinkContext
-      Component={'div'}
-      id={label}
-      href={href}>
-        <LinkItem {...props} />
-    </TrackedLinkContext>
+    <TrackedLink
+      className="footer__link-item"
+      id={makeIdFromString(item.label)}
+      to={normalizedTo}
+      {...props}
+    >
+      {item.label}
+    </TrackedLink>
   );
 }
