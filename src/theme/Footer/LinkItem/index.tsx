85edf11a06d0c3675899d85bdbaef83008814ff6
sdirosa <surai@objectiv.io>
2022-06-02 11:43:06 +0200

    Alternative implementation of Footer link items

--- src/theme/Footer/LinkItem/index.tsx
+++ src/theme/Footer/LinkItem/index.tsx
@@ -1,21 +1,22 @@
-import React, {ComponentProps} from 'react';
-import LinkItem from '@theme-original/Footer/LinkItem';
-import type LinkItemType from '@theme/Footer/LinkItem';
-import { TrackedLinkContext } from '@objectiv/tracker-react';
 import { makeIdFromString } from '@objectiv/tracker-core';
+import type LinkItemType from '@theme/Footer/LinkItem';
+import React, { ComponentProps } from 'react';
+import { TrackedLink } from "../../../trackedComponents/TrackedLink";
 
 type Props = ComponentProps<typeof LinkItemType>;
 
 export default function LinkItemWrapper(props: Props): JSX.Element {
   const {item} = props;
   const label = makeIdFromString(item.label);
-  const href = item.href;
+
   return (
-    <TrackedLinkContext
-      Component={'div'}
+    <TrackedLink
+      className="footer__link-item"
       id={label}
-      href={href}>
-        <LinkItem {...props} />
-    </TrackedLinkContext>
+      to={item.to}
+      {...props}
+    >
+      {item.label}
+    </TrackedLink>
   );
 }
