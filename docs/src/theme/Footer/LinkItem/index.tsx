c673c33dcf4a6ac8845e62560288e143a31faf1e
Ivar Pruijn <ivar@objectiv.io>
2022-06-02 11:07:08 +0200

    Use makeIdFromString for TrackedLinkContext

--- docs/src/theme/Footer/LinkItem/index.tsx
+++ docs/src/theme/Footer/LinkItem/index.tsx
@@ -2,12 +2,13 @@ import React, {ComponentProps} from 'react';
 import LinkItem from '@theme-original/Footer/LinkItem';
 import type LinkItemType from '@theme/Footer/LinkItem';
 import { TrackedLinkContext } from '@objectiv/tracker-react';
+import { makeIdFromString } from '@objectiv/tracker-core';
 
 type Props = ComponentProps<typeof LinkItemType>;
 
 export default function LinkItemWrapper(props: Props): JSX.Element {
   const {item} = props;
-  const label = item.label;
+  const label = makeIdFromString(item.label);
   const href = item.href;
   return (
     <TrackedLinkContext
