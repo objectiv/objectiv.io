8a776d8a1367c3152784d27d6a0a8f6d107e54e2
Ivar Pruijn <ivar@objectiv.io>
2022-06-02 13:45:26 +0200

    Fix navbar active item highlighting

--- docs/src/theme/NavbarItem/NavbarNavLink.tsx
+++ docs/src/theme/NavbarItem/NavbarNavLink.tsx
@@ -2,17 +2,20 @@ import React, {ComponentProps} from 'react';
 import NavbarNavLink from '@theme-original/NavbarItem/NavbarNavLink';
 import type NavbarNavLinkType from '@theme/NavbarItem/NavbarNavLink';
 import { TrackedLink } from '../../trackedComponents/TrackedLink';
+import {isRegexpStringMatch} from '@docusaurus/theme-common';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import isInternalUrl from '@docusaurus/isInternalUrl';
 
 type Props = ComponentProps<typeof NavbarNavLinkType>;
 
 export default function NavbarNavLinkWrapper(props: Props): JSX.Element {
-  const { label, href, to, prependBaseUrlToHref } = props;
+  const { label, href, to, prependBaseUrlToHref, activeBasePath, activeBaseRegex } = props;
   const toUrl = useBaseUrl(to);
+  const activeBaseUrl = useBaseUrl(activeBasePath);
   const normalizedHref = useBaseUrl(href, {forcePrependBaseUrl: true});
-  const linkTo = href ? (prependBaseUrlToHref ? normalizedHref : href) : toUrl;
   const isExternalLink = label && href && !isInternalUrl(href);
+
+  const linkTo = href ? (prependBaseUrlToHref ? normalizedHref : href) : toUrl;
   const waitUntilTracked = isExternalLink ? true : false;
 
   return (
@@ -21,6 +24,13 @@ export default function NavbarNavLinkWrapper(props: Props): JSX.Element {
       id={label as string} 
       href={linkTo} 
       waitUntilTracked={waitUntilTracked}
+      isNavLink
+      {...((activeBasePath || activeBaseRegex) && {
+        isActive: (_match, location) =>
+          activeBaseRegex
+            ? isRegexpStringMatch(activeBaseRegex, location.pathname)
+            : location.pathname.startsWith(activeBaseUrl),
+      })}      
       {...props} />
   );
 }
