db9ff31defca096013397ed177f46a2ea80e3601
Ivar Pruijn <ivar@objectiv.io>
2022-06-02 11:09:35 +0200

    Use makeIdFromString for TrackedLinkContext in all other components

--- docs/src/theme/DocSidebarItem/Link/index.tsx
+++ docs/src/theme/DocSidebarItem/Link/index.tsx
@@ -2,12 +2,13 @@ import React, {ComponentProps} from 'react';
 import Link from '@theme-original/DocSidebarItem/Link';
 import type LinkType from '@theme/DocSidebarItem/Link';
 import { TrackedLinkContext } from '@objectiv/tracker-react';
+import { makeIdFromString } from '@objectiv/tracker-core';
 
 type Props = ComponentProps<typeof LinkType>;
 
 export default function LinkWrapper(props: Props): JSX.Element {
   const { item } = props;
-  const label = item.label;
+  const label = makeIdFromString(item.label);
   const href = item.href;
   return (
     <TrackedLinkContext 
--- docs/src/theme/PaginatorNavLink/index.tsx
+++ docs/src/theme/PaginatorNavLink/index.tsx
@@ -2,6 +2,7 @@ import React, {ComponentProps} from 'react';
 import type PaginatorNavLinkType from '@theme/PaginatorNavLink';
 import PaginatorNavLink from '@theme-original/PaginatorNavLink';
 import { TrackedLinkContext } from '@objectiv/tracker-react';
+import { makeIdFromString } from '@objectiv/tracker-core';
 
 type Props = ComponentProps<typeof PaginatorNavLinkType>
 
@@ -10,7 +11,7 @@ export default function PaginatorNavLinkWrapper(props: Props): JSX.Element {
   return (
     <TrackedLinkContext 
       Component={'div'}
-      id={(title as string != "" ? title as string : "[no title]")}>
+      id={(title as string != "" ? makeIdFromString(title as string) : "[no title]")}>
         <PaginatorNavLink {...props} />
     </TrackedLinkContext>
   );
