bc3d8ea477499fe32c8dea5b24765bfc8191d19a
sdirosa <surai@objectiv.io>
2022-06-14 17:48:18 +0200

    Add alias to cookiebot and collector interceptors

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,8 +1,8 @@
 Cypress.Commands.add('interceptThirdParties', () => {
   cy.intercept('https://api.github.com/repos/objectiv/objectiv-analytics', { fixture: 'github.json' }).as('github');
   cy.intercept('https://vimeo.com/api/oembed.json*', { fixture: 'vimeo.json' }).as('vimeo');
-  cy.intercept('https://consentcdn.cookiebot.com/*', { fixture: 'cookiebot.json' });
-  cy.intercept('http://localhost:8081*', {status: 200});
+  cy.intercept('https://consentcdn.cookiebot.com/*', { fixture: 'cookiebot.json' }).as('cookiebot');
+  cy.intercept('http://localhost:8081*', {status: 200}).as('collector');
 })
 
 Cypress.Commands.add('waitForThirdParties', () => {
