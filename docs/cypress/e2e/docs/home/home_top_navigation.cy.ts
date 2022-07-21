d65d2f9bf4b9067d71732d165cf8e8889eb9d867
Ivar Pruijn <ivar@objectiv.io>
2022-07-21 13:49:13 +0200

    Test top nav, bottom nav, TOC, and EditThisPage footer

---
+++ docs/cypress/e2e/docs/home/home_top_navigation.cy.ts
@@ -0,0 +1,26 @@
+describe('Docs home: top navigation', () => {
+  it('Should track PressEvents on navigation items', () => {
+    // Intercept all async calls to avoid rate-limiting and other side effects
+    cy.intercept('https://consentcdn.cookiebot.com/*', { fixture: 'cookiebot.json' }).as('cookiebot');
+    cy.intercept('http://localhost:8081*', { status: 200 }).as('collector');
+
+    // Set CookieConsent
+    cy.setCookieConsent();
+
+    // Visit home page
+    cy.visit('/docs');
+
+    // We don't want any anchor to actually navigate. See support/commands.ts for how `preventDefault` has been made
+    cy.get('nav.navbar--fixed-top a.navbar__brand').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a').contains('Tracking').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a').contains('Modeling').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a').contains('Taxonomy').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a.navGitHub').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a.navSlack').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a.navTwitter').preventDefault().click();
+    cy.get('nav.navbar--fixed-top a.navHomepage').preventDefault().click();
+
+    // Verify recorded events
+    cy.objectiv().filterEvents('PressEvent').snapshotEvents();
+  })
+})
--- docs/snapshots.js
+++ docs/snapshots.js
@@ -35,5 +35,545 @@ module.exports = {
         ]
       }
     }
+  },
+  "Docs home: top navigation": {
+    "Should track PressEvents on navigation items": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "logo",
+                "href": "/docs/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#1"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "tracking",
+                "href": "/docs/tracking/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#2"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "modeling",
+                "href": "/docs/modeling/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#3"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "taxonomy",
+                "href": "/docs/taxonomy/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#4"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "github",
+                "href": "https://github.com/objectiv/objectiv-analytics"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#5"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "slack",
+                "href": "http://localhost:3000/join-slack"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#6"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "twitter",
+                "href": "https://twitter.com/objectiv_io"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#7"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "navbar-top"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "objectivio",
+                "href": "http://localhost:3000"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#8"
+          }
+        ]
+      }
+    }
+  },
+  "Docs home: bottom navigation": {
+    "Should track PressEvents on navigation items": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "footer"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "privacy-policy",
+                "href": "http://localhost:3000/privacy/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#1"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "footer"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "cookies",
+                "href": "http://localhost:3000/privacy/cookies/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#2"
+          }
+        ]
+      }
+    }
+  },
+  "Docs home: TOC": {
+    "Should track PressEvents on TOC items": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "toc"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "getting-started",
+                "href": "#getting-started"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#1"
+          }
+        ]
+      }
+    }
+  },
+  "Docs home: Footer Edit This Page": {
+    "Should track PressEvents on Edit This Page items": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "ContentContext",
+                "id": "edit-this-page"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "edit-docs-page",
+                "href": "https://github.com/objectiv/objectiv.io/edit/main/docs/docs/introduction.md"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#1"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "ContentContext",
+                "id": "edit-this-page"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "get-help",
+                "href": "http://localhost:3000/join-slack"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#2"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "ContentContext",
+                "id": "edit-this-page"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "submit-idea-or-bug-report",
+                "href": "https://github.com/objectiv/objectiv-analytics"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#3"
+          }
+        ]
+      }
+    }
   }
 }
