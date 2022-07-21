e76c8f3f4a5206ec0f20596c96a9a496d4325324
Ivar Pruijn <ivar@objectiv.io>
2022-07-21 14:59:37 +0200

    Add paginator, sidebar, and breadcrumbs tests for home

--- docs/snapshots.js
+++ docs/snapshots.js
@@ -575,5 +575,212 @@ module.exports = {
         ]
       }
     }
+  },
+  "Docs home: sidebar navigation": {
+    "Should track PressEvents on sidebar navigation items": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "docs-sidebar"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "introduction",
+                "href": "/docs/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/"
+              }
+            ],
+            "id": "PressEvent#1"
+          }
+        ]
+      }
+    }
+  },
+  "Docs home: paginator": {
+    "Should track PressEvents on the paginator in the docs page": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "doc-paginator-navigation"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "quickstart-guide",
+                "href": "/docs/home/quickstart-guide"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/home/quickstart-guide"
+              }
+            ],
+            "id": "PressEvent#1"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "doc-paginator-navigation"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "no-title",
+                "href": "/docs/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/home/quickstart-guide"
+              }
+            ],
+            "id": "PressEvent#2"
+          }
+        ]
+      }
+    }
+  },
+  "Docs home: Overview paginator": {
+    "Should track PressEvents on the paginator in the docs page": {
+      "1": {
+        "errors": [],
+        "events": [
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "doc-paginator-navigation"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "quickstart-guide",
+                "href": "/docs/home/quickstart-guide"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/home/quickstart-guide"
+              }
+            ],
+            "id": "PressEvent#1"
+          },
+          {
+            "_type": "PressEvent",
+            "location_stack": [
+              {
+                "_type": "RootLocationContext",
+                "id": "home"
+              },
+              {
+                "_type": "NavigationContext",
+                "id": "doc-paginator-navigation"
+              },
+              {
+                "_type": "LinkContext",
+                "id": "no-title",
+                "href": "/docs/"
+              }
+            ],
+            "global_contexts": [
+              {
+                "_type": "HttpContext",
+                "id": "http_context",
+                "referrer": "",
+                "user_agent": "mocked-user-agent",
+                "remote_address": null
+              },
+              {
+                "_type": "ApplicationContext",
+                "id": "objectiv-docs-dev"
+              },
+              {
+                "_type": "PathContext",
+                "id": "http://localhost:3000/docs/home/quickstart-guide"
+              }
+            ],
+            "id": "PressEvent#2"
+          }
+        ]
+      }
+    }
   }
 }
